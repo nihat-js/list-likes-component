a4bc4a8e19123a49d29ee1b23b7f4d4d4a3faa2b
nihat-js <abdullazadenihat@gmail.com>
2023-01-12 01:29:50 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.IO.Pipes;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> likes = new List<string>();
+
+            Console.WriteLine("Enter who liked your post");
+            String answer = Console.ReadLine();
+            
+            
+            while (answer != "")
+            {
+                likes.Add(answer);
+                answer = Console.ReadLine();    
+            }
+
+            if (likes.Count == 0) {
+                Console.WriteLine("Be first to like");
+            }else if (likes.Count < 3)
+            {
+                string friends = "";
+                foreach(string person in likes)
+                {
+                    friends += " " + person + ",";
+                }
+                Console.WriteLine(friends.Trim() + " liked you");
+            }else
+            {
+                Console.WriteLine(likes[0] + ", " + likes[1] + " and other " + (likes.Count - 2) + " liked you"); ;
+            }
+
+
+
+        }
+    }
+}
